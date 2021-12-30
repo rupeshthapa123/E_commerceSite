52aea69908b794a919b9dfbbdcfb7a4cb2e4eddc
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2021-12-30 19:33:54 +0545

    change

--- E_commerceSite/settings.py
+++ E_commerceSite/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 
-from pathlib import Path, django_heroku
+from pathlib import Path
+
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=  os.path.join(BASE_DIR,'assets')
+STATIC_ROOT=  os.path.join(BASE_DIR,'staticfiles')
 django_heroku.settings(locals())
 
 
